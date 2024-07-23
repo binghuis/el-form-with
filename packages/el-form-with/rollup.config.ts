1522496688e1831bca496201b04e0a71f694779c
宋秉徽 <451000936@qq.com>
2024-07-23 14:33:34 +0800

    feat: rollup

---
+++ packages/el-form-with/rollup.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "rollup";
+import esbuild from "rollup-plugin-esbuild";
+import { dts } from "rollup-plugin-dts";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { PackageJson } from "type-fest";
+import { readFileSync } from "node:fs";
+
+const { dependencies, peerDependencies } = JSON.parse(
+  readFileSync("./package.json", "utf8")
+) as PackageJson;
+
+const external = [
+  ...Object.keys(dependencies || {}),
+  ...Object.keys(peerDependencies || {}),
+];
+
+export default defineConfig([
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist",
+      format: "esm",
+    },
+    plugins: [vueJsx(), esbuild({ minify: true })],
+    external,
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.d.ts",
+      format: "esm",
+    },
+    plugins: [
+      dts({
+        // https://github.com/Swatinem/rollup-plugin-dts/issues/143
+        compilerOptions: { preserveSymlinks: false },
+      }),
+    ],
+    external,
+  },
+]);
